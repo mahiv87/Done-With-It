d3199ddd3f6f4acc113a4952579ceb9d9f6023d3
Marcus mbp <maherreraiv1987@gmail.com>
2022-09-04 18:51:37 -0600

    icons added to view image screen

--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Image, View, StyleSheet } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 import colors from '../config/colors';
 
@@ -7,8 +8,12 @@ function ViewImageScreen(props) {
 	return (
 		<View style={styles.background}>
 			<View style={styles.buttonContainer}>
-				<View style={styles.closeButton}></View>
-				<View style={styles.deleteButton}></View>
+				<View style={styles.closeButton}>
+					<MaterialCommunityIcons name="close" color="white" size={30} />
+				</View>
+				<View style={styles.deleteButton}>
+					<MaterialCommunityIcons name="trash-can-outline" color="white" size={35} />
+				</View>
 			</View>
 			<Image style={styles.image} resizeMode="contain" source={require('../assets/chair.jpg')} />
 		</View>
@@ -24,17 +29,11 @@ const styles = StyleSheet.create({
 		flexDirection: 'row'
 	},
 	closeButton: {
-		width: 50,
-		height: 50,
-		backgroundColor: colors.primary,
 		position: 'absolute',
 		top: 40,
 		left: 30
 	},
 	deleteButton: {
-		width: 50,
-		height: 50,
-		backgroundColor: colors.secondary,
 		position: 'absolute',
 		top: 40,
 		right: 30
