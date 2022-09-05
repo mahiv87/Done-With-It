f1c0faab281c93d09b1fc02ae0c0ae3ad6f7148c
Marcus mbp <maherreraiv1987@gmail.com>
2022-09-05 17:26:37 -0600

    delete messages working now

--- app/components/ListItemDeleteAction.js
+++ app/components/ListItemDeleteAction.js
@@ -3,12 +3,15 @@ import { View, StyleSheet } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 import colors from '../config/colors';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 
-function ListItemDeleteAction(props) {
+function ListItemDeleteAction({ onPress }) {
 	return (
-		<View style={styles.container}>
-			<MaterialCommunityIcons name="trash-can" color={colors.white} size={35} />
-		</View>
+		<TouchableWithoutFeedback onPress={onPress}>
+			<View style={styles.container}>
+				<MaterialCommunityIcons name="trash-can" color={colors.white} size={35} />
+			</View>
+		</TouchableWithoutFeedback>
 	);
 }
 
--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FlatList, StyleSheet, View } from 'react-native';
 
 import ListItem from '../components/ListItem';
@@ -6,7 +6,7 @@ import Screen from '../components/Screen';
 import ListItemSeparator from '../components/ListItemSeparator';
 import ListItemDeleteAction from '../components/ListItemDeleteAction';
 
-const messages = [
+const initialMessages = [
 	{
 		id: 1,
 		title: 'T1',
@@ -22,6 +22,14 @@ const messages = [
 ];
 
 function MessagesScreen(props) {
+	const [messages, setMessages] = useState(initialMessages);
+
+	const handleDelete = (message) => {
+		// Delete message from messages array
+		const newMessages = messages.filter((m) => m.id !== message.id);
+		setMessages(newMessages);
+	};
+
 	return (
 		<Screen>
 			<FlatList
@@ -33,7 +41,7 @@ function MessagesScreen(props) {
 						subTitle={item.description}
 						image={item.image}
 						onPress={() => console.log('Message selected', item)}
-						renderRightActions={ListItemDeleteAction}
+						renderRightActions={() => <ListItemDeleteAction onPress={() => handleDelete(item)} />}
 					/>
 				)}
 				ItemSeparatorComponent={ListItemSeparator}
