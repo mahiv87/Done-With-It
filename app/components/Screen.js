01c50b0747e6023d077d6cb69483a8dd945fbdbb
Marcus mbp <maherreraiv1987@gmail.com>
2022-09-05 17:32:34 -0600

    screen refresh on messages

--- app/components/Screen.js
+++ app/components/Screen.js
@@ -8,7 +8,8 @@ function Screen({ children }) {
 
 const styles = StyleSheet.create({
 	screen: {
-		paddingTop: Constants.statusBarHeight
+		paddingTop: Constants.statusBarHeight,
+		flex: 1
 	}
 });
 
