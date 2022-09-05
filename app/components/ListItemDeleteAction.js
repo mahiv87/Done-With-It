07e357f184034ce8a0ba74f567ac91cbf957f348
Marcus mbp <maherreraiv1987@gmail.com>
2022-09-04 20:02:22 -0600

    gesture functional component created

---
+++ app/components/ListItemDeleteAction.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+import colors from '../config/colors';
+
+function ListItemDeleteAction(props) {
+	return (
+		<View style={styles.container}>
+			<MaterialCommunityIcons name="trash-can" color={colors.white} size={35} />
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		backgroundColor: colors.danger,
+		width: 70,
+		justifyContent: 'center',
+		alignItems: 'center'
+	}
+});
+
+export default ListItemDeleteAction;
