5846ba2f1f1580d1a95dd8735d8ce7ffd76c09d6
Marcus mbp <maherreraiv1987@gmail.com>
2022-10-03 15:20:56 -0600

    refactor text style

--- app/components/AppText/styles.js
+++ app/components/AppText/styles.js
@@ -1,6 +1,9 @@
 import { StyleSheet, Platform } from 'react-native';
 
+import colors from '../../config/colors';
+
 const styles = StyleSheet.create({
+	colors,
 	text: {
 		fontSize: 18,
 		fontFamily: Platform.OS === 'android' ? 'Roboto' : 'Avenir'
