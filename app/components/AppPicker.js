20f1aca5a6d6a830601ac521bddfb6ca4b5ca7f3
Marcus mbp <maherreraiv1987@gmail.com>
2022-11-09 18:57:26 -0700

    listing screen

--- app/components/AppPicker.js
+++ app/components/AppPicker.js
@@ -17,7 +17,11 @@ function AppPicker({ icon, items, placeholder, onSelectItem, selectedItem }) {
 					{icon && (
 						<MaterialCommunityIcons name={icon} size={20} color={defaultStyles.colors.medium} style={styles.icon} />
 					)}
-					<AppText style={styles.text}>{selectedItem ? selectedItem.label : placeholder}</AppText>
+					{selectedItem ? (
+						<AppText style={styles.text}>{selectedItem.label}</AppText>
+					) : (
+						<AppText style={styles.placeholder}>{placeholder}</AppText>
+					)}
 					<MaterialCommunityIcons name="chevron-down" size={20} color={defaultStyles.colors.medium} />
 				</View>
 			</TouchableWithoutFeedback>
@@ -53,6 +57,10 @@ const styles = StyleSheet.create({
 	},
 	icon: {
 		marginRight: 10
+	},
+	placeholder: {
+		color: defaultStyles.colors.medium,
+		flex: 1
 	},
 	text: {
 		flex: 1
